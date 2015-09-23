6ff700fdfb33e69a4541e2629a356641cb0c460a
Yam Marcovic <ymarcov@gmail.com>
2015-09-23 13:56:19 +0300

    Make lexer an internal parser class

--- src/Parser.cc
+++ src/Parser.cc
@@ -1,131 +1,114 @@
 #include "Parser.h"
 
-#include <array>
-#include <initializer_list>
 #include <string.h>
 
 namespace Yam {
 namespace Http {
 
-namespace {
+Parser::Lexer::Lexer(const char* stream, std::size_t length) :
+    _stream{stream},
+    _initialLength{length},
+    _length{length} {}
 
-class Lexer {
-public:
-    Lexer(const char* stream, std::size_t length) :
-        _stream{stream},
-        _initialLength{length},
-        _length{length} {}
+void Parser::Lexer::SetDelimeters(std::initializer_list<const char*> delimeters) {
+    if (_delimeters.size() < delimeters.size())
+        throw std::logic_error("Too many delimeters");
 
-    void SetDelimeters(std::initializer_list<const char*> delimeters) {
-        if (_delimeters.size() < delimeters.size())
-            throw std::logic_error("Too many delimeters");
+    _delimeterCount = 0;
+    _usableDelimeterData = _delimeterData.size();
 
-        _delimeterCount = 0;
-        _usableDelimeterData = _delimeterData.size();
+    for (const char* d : delimeters)
+        AddDelimeter(d);
+}
 
-        for (const char* d : delimeters)
-            AddDelimeter(d);
+std::size_t Parser::Lexer::GetConsumption() const {
+    return _initialLength - _length;
+}
+
+bool Parser::Lexer::EndOfStream() const {
+    return !_length;
+}
+
+std::size_t Parser::Lexer::Compress() {
+    std::size_t stride = ConsumeDelimeters(_stream, 0);
+    _stream += stride;
+    _length -= stride;
+    return stride;
+}
+
+std::pair<const char*, std::size_t> Parser::Lexer::Next(bool compress) {
+    const char* startingPoint = _stream;
+    const char* cursor = startingPoint;
+    std::size_t wordLength = 0;
+
+    while (_length > wordLength) {
+        if (std::size_t delimLength = DelimeterAt(cursor, wordLength)) {
+            std::size_t stride = wordLength;
+
+            if (compress)
+                stride += ConsumeDelimeters(cursor, wordLength);
+            else
+                stride += delimLength;
+
+            _stream += stride;
+            _length -= stride;
+
+            return {startingPoint, wordLength};
+        } else {
+            ++cursor;
+            ++wordLength;
+        }
     }
 
-    std::size_t GetConsumption() const {
-        return _initialLength - _length;
-    }
+    // reached end of stream
 
-    bool EndOfStream() const {
-        return !_length;
-    }
+    _stream += wordLength;
+    _length -= wordLength;
 
-    std::size_t Compress() {
-        std::size_t stride = ConsumeDelimeters(_stream, 0);
-        _stream += stride;
-        _length -= stride;
-        return stride;
-    }
+    return {startingPoint, wordLength};
+}
 
-    std::pair<const char*, std::size_t> Next(bool compressDelimeters = true) {
-        const char* startingPoint = _stream;
-        const char* cursor = startingPoint;
-        std::size_t wordLength = 0;
+std::size_t Parser::Lexer::ConsumeDelimeters(const char* cursor, std::size_t consumed) {
+    std::size_t stride = 0;
 
-        while (_length > wordLength) {
-            if (std::size_t delimLength = DelimeterAt(cursor, wordLength)) {
-                std::size_t stride = wordLength;
-
-                if (compressDelimeters)
-                    stride += ConsumeDelimeters(cursor, wordLength);
-                else
-                    stride += delimLength;
-
-                _stream += stride;
-                _length -= stride;
-
-                return {startingPoint, wordLength};
-            } else {
-                ++cursor;
-                ++wordLength;
-            }
+    while (_length > consumed + stride)
+        if (auto delimLength = DelimeterAt(cursor, consumed + stride)) {
+            cursor += delimLength;
+            stride += delimLength;
+        } else {
+            break;
         }
 
-        // reached end of stream
+    return stride;
+}
 
-        _stream += wordLength;
-        _length -= wordLength;
+void Parser::Lexer::AddDelimeter(const char* delimeter) {
+    std::size_t length = ::strnlen(delimeter, _usableDelimeterData + 1);
 
-        return {startingPoint, wordLength};
-    }
+    if (length > _usableDelimeterData)
+        throw std::logic_error("Delimeter too large to fit buffer");
 
-private:
-    std::size_t ConsumeDelimeters(const char* cursor, std::size_t consumed) {
-        std::size_t stride = 0;
+    std::size_t dataOffset = _delimeterData.size() - _usableDelimeterData;
+    char* dataSlot = _delimeterData.data() + dataOffset;
+    ::strncpy(dataSlot, delimeter, length);
 
-        while (_length > consumed + stride)
-            if (auto delimLength = DelimeterAt(cursor, consumed + stride)) {
-                cursor += delimLength;
-                stride += delimLength;
-            } else {
-                break;
-            }
+    _delimeterLengths[_delimeterCount] = length;
+    _delimeters[_delimeterCount] = dataSlot;
 
-        return stride;
-    }
+    _usableDelimeterData -= length;
+    ++_delimeterCount;
 
-    void AddDelimeter(const char* delimeter) {
-        std::size_t length = ::strnlen(delimeter, _usableDelimeterData + 1);
+}
 
-        if (length > _usableDelimeterData)
-            throw std::logic_error("Delimeter too large to fit buffer");
+std::size_t Parser::Lexer::DelimeterAt(const char* cursor, std::size_t consumed) {
+    for (std::size_t i = 0; i < _delimeterCount; ++i)
+        if (_length > consumed + (_delimeterLengths[i] - 1))
+            if (!::strncmp(cursor, _delimeters[i], _delimeterLengths[i]))
+                return _delimeterLengths[i];
+    return 0;
+}
 
-        std::size_t dataOffset = _delimeterData.size() - _usableDelimeterData;
-        char* dataSlot = _delimeterData.data() + dataOffset;
-        ::strncpy(dataSlot, delimeter, length);
-
-        _delimeterLengths[_delimeterCount] = length;
-        _delimeters[_delimeterCount] = dataSlot;
-
-        _usableDelimeterData -= length;
-        ++_delimeterCount;
-
-    }
-
-    std::size_t DelimeterAt(const char* cursor, std::size_t consumed) {
-        for (std::size_t i = 0; i < _delimeterCount; ++i)
-            if (_length > consumed + (_delimeterLengths[i] - 1))
-                if (!::strncmp(cursor, _delimeters[i], _delimeterLengths[i]))
-                    return _delimeterLengths[i];
-        return 0;
-    }
-
-    const char* _stream;
-    std::size_t _initialLength;
-    std::size_t _length;
-    std::array<const char*, 16> _delimeters;
-    std::array<std::size_t, 16> _delimeterLengths;
-    std::array<char, 64> _delimeterData;
-    std::size_t _delimeterCount = 0;
-    std::size_t _usableDelimeterData = 0;
-};
-
-} // unnamed namespace
+/* Parser functions */
 
 Parser::Parser(const char* buf, std::size_t bufSize) :
     _positionedBuffer(buf),
