d3057fb71124f350b3d34ae900256cf05d801f8d
Yam Marcovic <ymarcov@gmail.com>
2015-12-02 21:13:55 +0200

    Sandbox improvements

--- sandbox/main.cc
+++ sandbox/main.cc
@@ -1,27 +1,104 @@
+#include "MemoryPool.h"
+#include "Request.h"
 #include "TcpServer.h"
 
 #include <iostream>
+#include <memory>
 #include <string>
 
 using namespace Yam::Http;
 
-int main(int argc, char* argv[]) {
+struct ServerConfiguration {
+    IPEndpoint _endpoint;
+    std::shared_ptr<ThreadPool> _threadPool;
+};
+
+std::unique_ptr<TcpServer> CreateServer(ServerConfiguration config) {
+    return std::make_unique<TcpServer>(config._endpoint, config._threadPool);
+}
+
+ServerConfiguration CreateConfiguration(std::vector<std::string> argv) {
+    if (argv.size() > 2)
+        throw std::runtime_error("Invalid command line arguments");
+
+    auto port = 3000;
+    if (argv.size() > 1)
+        port = std::stoi(argv[1]);
+
+    auto endpoint = IPEndpoint{{127, 0, 0, 1}, port};
+
     int threadCount = 1;
-
-    if (argc > 1)
-        threadCount = std::stoi(argv[1]);
-
-    std::cout << "Sandbox started.\n";
+    if (argv.size() == 3)
+        threadCount = std::stoi(argv[2]);
 
     auto threadPool = std::make_shared<ThreadPool>(threadCount);
-    auto endpoint = IPEndpoint{{127, 0,0, 1}, 3000};
 
-    TcpServer server{endpoint, threadPool};
+    return {endpoint, threadPool};
+}
 
-    auto handler = [](std::shared_ptr<TcpConnection> conn) {
+void PrintInfo(const Request& request) {
+    /*
+     * Print request line
+     */
+
+    switch (request.GetMethod()) {
+        case Protocol::Method::Get:
+            std::cout << "GET ";
+            break;
+        case Protocol::Method::Head:
+            std::cout << "HEAD ";
+            break;
+        case Protocol::Method::Post:
+            std::cout << "POST ";
+            break;
+    }
+
+    std::cout << request.GetUri() << " ";
+
+    switch (request.GetProtocol()) {
+        case Protocol::Version::Http10:
+            std::cout << "HTTP/1.0\n";
+            break;
+        case Protocol::Version::Http11:
+            std::cout << "HTTP/1.1\n";
+            break;
+    }
+
+    /*
+     * Print cookies
+     */
+
+    for (auto& cookieName : request.GetCookieNames()) {
+        auto cookie = request.GetCookie(cookieName);
+        std::cout << "Cookie: " << cookieName << " = " << cookie << "\n";
+    }
+}
+
+TcpServer::ConnectionHandler CreateHandler() {
+    auto memoryPool = MemoryPool<Request::Buffer>::Create();
+
+    return [=](std::shared_ptr<TcpConnection> conn) {
         std::cout << "Connection received!\n";
+
+        auto buffer = memoryPool->New();
+        conn->Read(buffer.get(), sizeof(Request::Buffer));
+        auto request = Request{std::move(buffer)};
+
+        PrintInfo(request);
         conn->Write("hello!\n", 8);
     };
+}
 
-    server.Start(handler).get();
+std::vector<std::string> ArgsToVector(int argc, char* argv[]) {
+    return std::vector<std::string>{argv, argv + argc};
+}
+
+int main(int argc, char* argv[]) {
+    auto args = ArgsToVector(argc, argv);
+    auto config = CreateConfiguration(args);
+    auto server = CreateServer(config);
+
+    auto task = server->Start(CreateHandler());
+    std::cout << "Sandbox started.\n";
+    task.get();
 }
