2781c068af0be9a28ec2674fe8b7b958e4907215
Yam Marcovic <ymarcov@gmail.com>
2015-12-15 23:01:22 +0200

    Fix code to enable optimization with g++

--- src/TcpConnection.cc
+++ src/TcpConnection.cc
@@ -28,7 +28,7 @@ void Connect(Socket& socket, const IPEndpoint& ep) {
 } // unnamed namespace
 
 IPEndpoint::IPEndpoint(const ::sockaddr_in& sa) {
-    int port = ::ntohs(sa.sin_port);
+    int port = ntohs(sa.sin_port);
     auto ip = reinterpret_cast<const std::uint8_t*>(&sa.sin_addr.s_addr);
     _address = {ip[0], ip[1], ip[2], ip[3]};
     _port = port;
@@ -39,7 +39,7 @@ std::unique_ptr<::sockaddr_in> IPEndpoint::GetAddrInfo() const {
     std::memset(addr.get(), 0, sizeof(*addr));
 
     addr->sin_family = AF_INET;
-    addr->sin_port = ::htons(_port);
+    addr->sin_port = htons(_port);
     auto ip = *reinterpret_cast<const std::uint32_t*>(_address.data());
     addr->sin_addr.s_addr = ip;
 
