ef22ffa3fb37a8d85cf23085cbc8d567c76624df
Yam Marcovic <ymarcov@gmail.com>
2016-01-11 22:31:10 +0200

    Add BackTrace to SystemError

--- include/SystemError.h
+++ include/SystemError.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "BackTrace.h"
+
 #include <cerrno>
 #include <system_error>
 
@@ -10,6 +12,17 @@ class SystemError : public std::system_error {
 public:
     SystemError() :
         system_error(errno, std::system_category()) {}
+
+    std::string GetMessage() const {
+        return what();
+    }
+
+    const BackTrace& GetBackTrace() const {
+        return _backTrace;
+    }
+
+private:
+    BackTrace _backTrace;
 };
 
 } // namespace Http
