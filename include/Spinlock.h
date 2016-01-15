3573547c5f1060b6e1f9899c95d7c60d256cf582
Yam Marcovic <ymarcov@gmail.com>
2016-01-15 17:15:41 +0200

    Add Spinlock.
    Unused for now... but I wrote it wanting to try it out
    for the MemoryPool, instead of std::mutex... Turns out
    spinning isn't really adding anything here.

---
+++ include/Spinlock.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <atomic>
+
+namespace Yam {
+namespace Http {
+
+/**
+ * Just a regular spinlock.
+ *
+ * Satisfies the Mutex concept, and
+ * can be used with std::lock_guard.
+ */
+class Spinlock {
+public:
+    Spinlock();
+    Spinlock(const Spinlock&) = delete;
+
+    bool TryLock(std::size_t jiffies = 1000);
+    void Lock();
+    void Unlock();
+
+    void lock();
+    void unlock();
+
+private:
+    std::atomic_flag _flag;
+};
+
+inline Spinlock::Spinlock() :
+    _flag{ATOMIC_FLAG_INIT} {}
+
+inline void Spinlock::lock() {
+    Lock();
+}
+
+inline void Spinlock::unlock() {
+    Unlock();
+}
+
+inline bool Spinlock::TryLock(std::size_t jiffies) {
+    while (--jiffies)
+        if (!_flag.test_and_set(std::memory_order_acquire))
+            return true;
+    return !_flag.test_and_set(std::memory_order_acquire);
+}
+
+inline void Spinlock::Lock() {
+    while (_flag.test_and_set(std::memory_order_acquire))
+        ;
+}
+
+inline void Spinlock::Unlock() {
+    _flag.clear(std::memory_order_release);
+}
+
+} // namespace Http
+} // namespace Yam
+
