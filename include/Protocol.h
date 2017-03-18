29e5ce41a4dafcd25b547740dc14a3f02bc7c1cc
Yam Marcovic <ymarcov@gmail.com>
2017-03-18 15:45:34 +0200

    Improve doxygen for Protocol namespace

--- include/Protocol.h
+++ include/Protocol.h
@@ -7,82 +7,150 @@
 
 namespace Yam {
 namespace Http {
+
+/**
+ * Test
+ */
 inline namespace Protocol {
 
+/**
+ * HTTP Protocol related enums and classes.
+ */
 enum class Method {
-    Options,
-    Get,
-    Head,
-    Post,
-    Put,
-    Delete,
-    Trace,
-    Connect
+    Options, ///< OPTIONS
+    Get, ///< GET
+    Head, ///< HEAD
+    Post, ///< POST
+    Put, ///< PUT
+    Delete, ///< DELETE
+    Trace, ///< TRACE
+    Connect ///< CONNECT
 };
 
+/**
+ * An HTTP request/response version.
+ */
 enum class Version {
-    Http10,
-    Http11
+    Http10, ///< HTTP/1.0
+    Http11 ///< HTTP/1.1
 };
 
+/**
+ * An HTTP response status.
+ */
 enum class Status {
-    Continue, // 100
-    SwitchingProtocols, // 101
-    Ok, // 200
-    Created, // 201
-    Accepted, // 202
-    NonAuthoritativeInformation, // 203
-    NoContent, // 204
-    ResetContent, // 205
-    PartialContent, // 206
-    MultipleChoices, // 300
-    MovedPermanently, // 301
-    Found, // 302
-    SeeOther, // 303
-    NotModified, // 304
-    UseProxy, // 305
+    Continue, ///< 100
+    SwitchingProtocols, ///< 101
+    Ok, ///< 200
+    Created, ///< 201
+    Accepted, ///< 202
+    NonAuthoritativeInformation, ///< 203
+    NoContent, ///< 204
+    ResetContent, ///< 205
+    PartialContent, ///< 206
+    MultipleChoices, ///< 300
+    MovedPermanently, ///< 301
+    Found, ///< 302
+    SeeOther, ///< 303
+    NotModified, ///< 304
+    UseProxy, ///< 305
     // 306 is reserved
-    TemporaryRedirect, // 307
-    BadRequest, // 400
-    Unauthorized, // 401
+    TemporaryRedirect, ///< 307
+    BadRequest, ///< 400
+    Unauthorized, ///< 401
     // 402 is reserved
-    Forbidden, // 403
-    NotFound, // 404
-    MethodNotAllowed, // 405
-    NotAcceptable, // 406
-    ProxyAuthenticationRequired, // 407
-    RequestTimeout, // 408
-    Conflict, // 409
-    Gone, // 410
-    LengthRequired, // 411
-    PreconditionFailed, // 412
-    RequestEntityTooLarge, // 413
-    RequestUriTooLong, // 414
-    UnsupportedMediaType, // 415
-    RequestedRangeNotSatisfiable, // 416
-    ExpectationFailed, // 417
-    InternalServerError, // 500
-    NotImplemented, // 501
-    BadGateway, // 502
-    ServiceUnavailable, // 503
-    GatewayTimeout, // 504
-    HttpVersionNotSupported // 505
+    Forbidden, ///< 403
+    NotFound, ///< 404
+    MethodNotAllowed, ///< 405
+    NotAcceptable, ///< 406
+    ProxyAuthenticationRequired, ///< 407
+    RequestTimeout, ///< 408
+    Conflict, ///< 409
+    Gone, ///< 410
+    LengthRequired, ///< 411
+    PreconditionFailed, ///< 412
+    RequestEntityTooLarge, ///< 413
+    RequestUriTooLong, ///< 414
+    UnsupportedMediaType, ///< 415
+    RequestedRangeNotSatisfiable, ///< 416
+    ExpectationFailed, ///< 417
+    InternalServerError, ///< 500
+    NotImplemented, ///< 501
+    BadGateway, ///< 502
+    ServiceUnavailable, ///< 503
+    GatewayTimeout, ///< 504
+    HttpVersionNotSupported ///< 505
 };
 
+/**
+ * Extra options to specify for cookies.
+ */
 class CookieOptions {
 public:
+    /**
+     * Sets the cookie's domain.
+     */
     void SetDomain(const std::string&);
+
+    /**
+     * Sets the cookie's path.
+     */
     void SetPath(const std::string&);
+
+    /**
+     * Sets the max age of the cookie.
+     */
     void SetMaxAge(const std::chrono::seconds&);
+
+    /**
+     * Sets the expiration time of the cookie.
+     */
     void SetExpiration(const std::time_t&);
+
+    /**
+     * Sets whether the cookie is to be accessed
+     * only by the server, and not from client side scripts.
+     *
+     * (This is provided here, but its use in server code is questionable)
+     */
     void SetHttpOnly(bool = true);
+
+    /**
+     * Sets whether the cookie may only be sent
+     * over HTTPS connections.
+     */
     void SetSecure(bool = true);
 
+    /**
+     * Gets the cookie's domain.
+     */
     bool GetDomain(std::string*) const;
+
+    /**
+     * Gets the cookie's path.
+     */
     bool GetPath(std::string*) const;
+
+    /**
+     * Gets the max age of the cookie.
+     */
     bool GetMaxAge(std::chrono::seconds*) const;
+
+    /**
+     * Gets the cookie's expiration time.
+     */
     bool GetExpiration(std::time_t*) const;
+
+    /**
+     * Gets whether the cookie may only be
+     * accessed by the server.
+     */
     bool IsHttpOnly() const;
+
+    /**
+     * Gets whether the cookie may only be sent
+     * over HTTPS connections.
+     */
     bool IsSecure() const;
 
 private:
