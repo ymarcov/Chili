436825a7210cbdb8734dd9b55fb5eb384b46d166
Yam Marcovic <ymarcov@gmail.com>
2017-03-26 11:47:10 +0300

    Add InputStream documentation

--- include/InputStream.h
+++ include/InputStream.h
@@ -6,11 +6,31 @@
 namespace Yam {
 namespace Http {
 
+/**
+ * A stream of bytes.
+ *
+ * This can be used for streaming content from the server.
+ */
 class InputStream {
 public:
-    virtual std::size_t Read(void*, std::size_t) = 0;
-    virtual std::size_t Read(void* buffer, std::size_t maxBytes, std::chrono::milliseconds timeout);
+    /**
+     * Reads @p n bytes into @p buffer.
+     *
+     * @param buffer The buffer to read into.
+     * @param n      Maximum number of bytes to read.
+     * @return       The number of bytes actually read into the buffer.
+     */
+    virtual std::size_t Read(void* buffer, std::size_t n) = 0;
+
+    /**
+     * Returns true if the end of the stream has been reached.
+     */
     virtual bool EndOfStream() const = 0;
+
+    /**
+     * @internal
+     */
+    virtual std::size_t Read(void* buffer, std::size_t maxBytes, std::chrono::milliseconds timeout);
 
     virtual ~InputStream() = default;
 };
