6bdbe24e933904e2dcf0cf5e9aac0e424ec14a5d
Yam Marcovic <ymarcov@gmail.com>
2018-04-26 12:41:44 -0700

    Lower epoll max events per wait call

--- src/Poller.cc
+++ src/Poller.cc
@@ -106,7 +106,7 @@ void Poller::Stop() {
 }
 
 void Poller::PollLoop(const Poller::EventHandler& handler) {
-    constexpr int maxEvents = 1000;
+    constexpr int maxEvents = 100;
     struct epoll_event events[maxEvents];
 
     while (!_stop) {
